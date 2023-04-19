965d7a92e41e3babcb9ca80ae9c884e9c340db03
Comrade X <106185020+comrade09@users.noreply.github.com>
2023-04-19 12:55:33 +0530

    Create lec.py

---
+++ plugins/lec.py
@@ -0,0 +1,18 @@
+import os
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.enums import ParseMode
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+from bot import Bot
+from helper_func import subscribed, encode, decode, get_messages
+
+
+LEC = f"hi gyus <a href= https://t.me/ath2023> hemlo </a>"
+
+@Bot.on_message(filters.command('lecture') & filters.private )
+async def lectue(bot: Bot, message: Message):
+    await message.reply_text( text = LEC,
+    disable_web_page_preview = True
+   )
+
