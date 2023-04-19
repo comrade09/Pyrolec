fe546d9d45abdce768d9fa01c38b0502fed1ba03
Comrade X <106185020+comrade09@users.noreply.github.com>
2023-04-19 13:29:26 +0530

    Update lec.py

--- plugins/lec.py
+++ plugins/lec.py
@@ -10,9 +10,8 @@ from helper_func import subscribed, encode, decode, get_messages
 
 LEC = f"hi gyus <a href= https://t.me/ath2023> hemlo </a>"
 
-@Bot.on_message(filters.command('lecture') & filters.private )
-async def lectue_command(bot: Bot, message: Message):
+@Bot.on_message(filters.command('lecture') & filters.private, group=234)
+async def lectue(bot: Bot, message: Message):
     await message.reply_text( text = LEC,
     disable_web_page_preview = True
    )
-
